23c5ffc23867b3dc2a25760c2d27285505247f30
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-10-30 13:39:02 +0500

    MoveFile

--- app/src/main/java/com/aarafrao/calculatorapp/Activities/HiddenActivity2.java
+++ app/src/main/java/com/aarafrao/calculatorapp/Activities/HiddenActivity2.java
@@ -29,8 +29,8 @@ import java.util.ArrayList;
 public class HiddenActivity2 extends AppCompatActivity
         implements View.OnClickListener {
     FloatingActionButton floatingActionButton;
-    // this is the action code we use in our intent,
-    // this way we know we're looking at the response from our own action
+    String currentPath = "";
+
     private static final int SELECT_SINGLE_PICTURE = 101;
     private static final int SELECT_MULTIPLE_PICTURE = 201;
     public static final String IMAGE_TYPE = "image/*";
@@ -77,6 +77,7 @@ public class HiddenActivity2 extends AppCompatActivity
     private void initId() {
 
         floatingActionButton = findViewById(R.id.floatingActionButton);
+        selectedImagePreview = findViewById(R.id.selectedImagePreview);
     }
 
     private void createFolder(String name) {
@@ -85,7 +86,7 @@ public class HiddenActivity2 extends AppCompatActivity
             mydir.mkdirs();
             Toast.makeText(this, "Created" + " -> " + mydir.getAbsolutePath(), Toast.LENGTH_SHORT).show();
         } else {
-
+            currentPath = mydir.getPath();
         }
     }
 
@@ -102,63 +103,28 @@ public class HiddenActivity2 extends AppCompatActivity
         Intent intent = new Intent();
         intent.setType(IMAGE_TYPE);
         intent.setAction(Intent.ACTION_GET_CONTENT);
-         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         startActivityForResult(Intent.createChooser(intent,
                 "Select Picture"), SELECT_MULTIPLE_PICTURE);
     }
 
-    public String getPath(Uri uri) {
-
-        // just some safety built in
-        if (uri == null) {
-            // perform some logging or show user feedback
-            Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_LONG).show();
-            Log.d(MainActivity.class.getSimpleName(), "Failed to parse image path from image URI " + uri);
-            return null;
-        }
-
-        // try to retrieve the image from the media store first
-        // this will only work for images selected from gallery
-        String[] projection = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        if (cursor != null) {
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        }
-        // this is our fallback here, thanks to the answer from @mad indicating this is needed for
-        // working code based on images selected using other file managers
-        return uri.getPath();
-    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_SINGLE_PICTURE) {
-
-                Uri selectedImageUri = data.getData();
-                try {
-                    selectedImagePreview.setImageBitmap(new UserPicture(selectedImageUri, getContentResolver()).getBitmap());
-                } catch (IOException e) {
-                    Log.e(MainActivity.class.getSimpleName(), "Failed to load image", e);
-                }
-
-            } else if (requestCode == SELECT_MULTIPLE_PICTURE) {
-                //And in the Result handling check for that parameter:
+            if (requestCode == SELECT_MULTIPLE_PICTURE) {
                 if (Intent.ACTION_SEND_MULTIPLE.equals(data.getAction())
                         && data.hasExtra(Intent.EXTRA_STREAM)) {
-                    // retrieve a collection of selected images
+
                     ArrayList<Parcelable> list = data.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-                    // iterate over these images
                     if (list != null) {
                         for (Parcelable parcel : list) {
                             Uri uri = (Uri) parcel;
-                            // handle the images one by one here
+                            moveFiles(uri);
+
                         }
                     }
 
-                    // for now just show the last picture
                     if (!list.isEmpty()) {
                         Uri imageUri = (Uri) list.get(list.size() - 1);
 
@@ -175,6 +141,12 @@ public class HiddenActivity2 extends AppCompatActivity
             Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_LONG).show();
             Log.d(MainActivity.class.getSimpleName(), "Failed to get intent data, result code is " + resultCode);
         }
+    }
+
+    private void moveFiles(Uri uri) {
+        File from = new File(String.valueOf(uri));
+        File to = new File(currentPath);
+        from.renameTo(to);
     }
 
 }
